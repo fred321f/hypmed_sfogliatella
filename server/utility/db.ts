3491d85ff00406a9811433582517abed08b72424
fred321f <frederick01nielsen@gmail.com>
2025-04-10 11:32:33 +0200

    change file structure

---
+++ server/utility/db.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGO_URI;
+if (!uri) throw new Error('MONGO_URI is missing in .env');
+
+const client = new MongoClient(uri);
+let db: any; // Store the database instance
+
+export async function connectDB() {
+    if (!db) {
+        await client.connect();
+        db = client.db('studio'); // Replace with your DB name
+        console.log('✅ Connected to MongoDB');
+    }
+    return db;
+}
