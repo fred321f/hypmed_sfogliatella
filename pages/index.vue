b670b9e83ef9a1cb4441052e01ed562ec5895c20
SvevaZ <svevazanetti.sz@gmail.com>
2025-04-26 13:17:02 +0200

    added section lines and first activities card to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,43 @@
     </div>
     <div class="container">
         <h2>
-            Welcome to a space where mind, body, and soul come together.
-            Discover balance, strength, and serenity at our yoga center.
+        Welcome to a space where mind, body, and soul come together.
+        Discover balance, strength, and serenity at our yoga center.
         </h2>
         <p>
             Yogatella is a well-established yoga center that has been offering the highest quality services in Milan for many years. With a deep passion for well-being and a commitment to excellence, we provide a welcoming space for all to explore the transformative power of yoga.
         </p>
         <button type="button" class="btn btn-primary">Learn more about the center</button>
+    </div>
+   
+    <div class="section-line">
+        <h2 class="section-title">OUR ACTIVITIES</h2>
+    </div>
+    <div class="container">
+        <div class="card mb-3" style="margin: 0 auto;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Activities</h5>
+                    <p class="card-text">We offer a wide variety of activities, ranging from yoga and meditation courses, retreats, seminars and Workshops</p>
+                    <a href="#" class="btn btn-primary">Learn more</a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="section-line">
+        <h2 class="section-title">HIGHLIGHTS</h2>
+    </div>
+    <div class="section-line">
+        <h2 class="section-title">OUR TEAHCERS</h2>
+    </div>
+    <div class="section-line">
+        <h2 class="section-title">JOIN US</h2>
     </div>
   
 </template>
@@ -18,14 +48,28 @@
 <style scoped>
 .header-line {
   width: 100%;
-  background-color: #d2f4ea; /* cambia il colore che vuoi */
-  padding: 20px 0; /* spazio sopra e sotto il titolo */
+  background-color: #d2f4ea; 
+  padding: 20px 0; 
   text-align: center;
 }
 
 .header-title {
-  color: black; /* colore del testo */
+  color: black; 
   margin: 0;
   font-size: 2rem;
+}
+
+.section-line {
+  width: 100%;
+  background-color: #d2f4ea; 
+  padding: 5px 0; 
+  text-align: center;
+  margin: 20px 0;
+}
+
+.section-title {
+  color: black; 
+  margin: 0;
+  font-size: 1.5rem;
 }
 </style>
