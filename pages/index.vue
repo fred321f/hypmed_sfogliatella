2332613e6a1850ca3b7ed327f0d809b28d529970
SvevaZ <svevazanetti.sz@gmail.com>
2025-04-27 13:05:30 +0200

    Activities and teachers card in homepage

--- pages/index.vue
+++ pages/index.vue
@@ -6,83 +6,40 @@
         </h2>
         <p class="mb-3 text-center">Yogatella is a well-established yoga center that has been offering the highest quality services in Milan for many years. With a deep passion for well-being and a commitment to excellence, we provide a welcoming space for all to explore the transformative power of yoga.
         </p>
-    </div>
-</template>
+        <div class="mb-3 text-center">
+            <a href="/about" type="button" class="btn btn-primary" >Learn more about the center</a>
+        </div>
+        
 
-<!-- 
-<template>
-    <div class="header-line">
-        <h1 class="header-title">Yogatella</h1>
-    </div>
-    <div class="container">
-        <h2>
-        Welcome to a space where mind, body, and soul come together.
-        Discover balance, strength, and serenity at our yoga center.
-        </h2>
-        <p>
-            Yogatella is a well-established yoga center that has been offering the highest quality services in Milan for many years. With a deep passion for well-being and a commitment to excellence, we provide a welcoming space for all to explore the transformative power of yoga.
-        </p>
-        <button type="button" class="btn btn-primary">Learn more about the center</button>
-    </div>
-   
-    <div class="section-line">
-        <h2 class="section-title">OUR ACTIVITIES</h2>
-    </div>
-    <div class="container">
-        <div class="card mb-3" style="margin: 0 auto;">
+        <div class="card mb-3" >
             <div class="row g-0">
                 <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="...">
+                <img src="https://www.yoss.it/wp-content/uploads/2020/03/ro147-scaled-1440x809.jpg" class="img-fluid rounded-start" alt="Person doing yoga">
                 </div>
                 <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">Activities</h5>
-                    <p class="card-text">We offer a wide variety of activities, ranging from yoga and meditation courses, retreats, seminars and Workshops</p>
-                    <a href="#" class="btn btn-primary">Learn more</a>
-                </div>
+                  <div class="card-body ms-3">
+                      <h5 class="card-title">Activities</h5>
+                      <p class="card-text">We offer a wide variety of activities, ranging from yoga and meditation courses, retreats, seminars and workshops</p>
+                      <a href="/activities" class="btn btn-primary">Learn more</a>
+                  </div>
                 </div>
             </div>
         </div>
+        <div class="card mb-3" >
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src="https://gorgeoustip.com/wp-content/uploads/2025/02/Yoga-teacher-training.jpg" class="img-fluid rounded-start" alt="Group of yoga teachers">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body ms-3">
+                      <h5 class="card-title">Teachers</h5>
+                      <p class="card-text">Meet the team! They are all certified and ready to welcome you in the yoga community </p>
+                      <a href="teachers" class="btn btn-primary">Learn more</a>
+                  </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-    
-    <div class="section-line">
-        <h2 class="section-title">HIGHLIGHTS</h2>
-    </div>
-    <div class="section-line">
-        <h2 class="section-title">OUR TEAHCERS</h2>
-    </div>
-    <div class="section-line">
-        <h2 class="section-title">JOIN US</h2>
-    </div>
-  
 </template>
 
-<style scoped>
-.header-line {
-  width: 100%;
-  background-color: #d2f4ea; 
-  padding: 20px 0; 
-  text-align: center;
-}
-
-.header-title {
-  color: black; 
-  margin: 0;
-  font-size: 2rem;
-}
-
-.section-line {
-  width: 100%;
-  background-color: #d2f4ea; 
-  padding: 5px 0; 
-  text-align: center;
-  margin: 20px 0;
-}
-
-.section-title {
-  color: black; 
-  margin: 0;
-  font-size: 1.5rem;
-}
-</style>
--->
