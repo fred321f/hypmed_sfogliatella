ea1479f2b48ec6719bfd98313b939ac2fc6c096a
fred321f <frederick01nielsen@gmail.com>
2025-04-29 08:54:06 +0200

    Updated titles and added extra section

--- pages/teachers/index.vue
+++ pages/teachers/index.vue
@@ -4,10 +4,13 @@ import Teachers from '~/components/TeachersList.vue';
 
 <template>
     <div class="container">
-        <h1 class="my-5 text-center display-2">Teachers</h1>
+        <h1 class="my-5 text-center display-1">Teachers</h1>
         <p class="mb-5 text-center lead">Meet our dedicated and experienced teachers who are passionate about sharing
             their knowledge and skills with you.
         </p>
         <Teachers />
+        <h2 class="my-3 display-4">
+            Meet the team
+        </h2>
     </div>
 </template>
