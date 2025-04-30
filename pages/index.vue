6c3b976d02debc1b1f13976eb8a6087cba3fb0ca
fred321f <frederick01nielsen@gmail.com>
2025-04-30 14:04:41 +0200

    changed titles in homepage and change gradient in global.css.

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="mb-3 text-center container">
-        <h1 class="my-5 text-center display-2">YogaTella</h1>
+        <h1 class="my-5 text-center display-1">YogaTella</h1>
         <h2 class="my-3 text-center lead">Welcome to a space where mind, body, and soul come together.
             Discover balance, strength, and serenity at our yoga center.
         </h2>
-        <img src="https://res.cloudinary.com/dpba22oef/image/upload/v1745954116/header-yoga-business-in-a-nutshell-considerations-on-opening-your-own-yoga-studio_z7ymxo.jpg" class="img-fluid rounded" alt="People doing yoga">
+        <img src="https://res.cloudinary.com/dpba22oef/image/upload/v1745954116/header-yoga-business-in-a-nutshell-considerations-on-opening-your-own-yoga-studio_z7ymxo.jpg" class="rounded img-fluid" alt="People doing yoga">
         <p class="my-3 text-center">Yogatella is a well-established yoga center that has been offering the highest quality services in Milan for many years. With a deep passion for well-being and a commitment to excellence, we provide a welcoming space for all to explore the transformative power of yoga.
         </p>
         <div class="my-3 text-center">
@@ -12,13 +12,13 @@
         </div>
     </div>
     <div class="my-5 container">
-        <div class="card mb-3" >
+        <div class="mb-3 card" >
             <div class="row g-0">
                 <div class="col-md-4">
-                <img src="https://res.cloudinary.com/dpba22oef/image/upload/w_1500,ar_3:2,c_fill,g_auto/v1745953332/ro147-scaled-1440x809-2_ygeojc.jpg" class="img-fluid rounded-start" alt="Person doing yoga">
+                <img src="https://res.cloudinary.com/dpba22oef/image/upload/w_1500,ar_3:2,c_fill,g_auto/v1745953332/ro147-scaled-1440x809-2_ygeojc.jpg" class="rounded-start img-fluid" alt="Person doing yoga">
                 </div>
                 <div class="col-md-8">
-                  <div class="card-body ms-3">
+                  <div class="ms-3 card-body">
                       <h5 class="card-title">Activities</h5>
                       <p class="card-text">We offer a wide variety of activities, ranging from yoga and meditation courses, retreats, seminars and workshops</p>
                       <a href="/activities" class="btn btn-primary">Learn more</a>
@@ -26,13 +26,13 @@
                 </div>
             </div>
         </div>
-        <div class="card mb-3" >
+        <div class="mb-3 card" >
             <div class="row g-0">
                 <div class="col-md-4">
-                <img src="https://res.cloudinary.com/dpba22oef/image/upload/w_1500,ar_3:2,c_fill/v1745953300/Yoga-teacher-training-2_ryrg6y.jpg" class="img-fluid rounded-start" alt="Group of yoga teachers">
+                <img src="https://res.cloudinary.com/dpba22oef/image/upload/w_1500,ar_3:2,c_fill/v1745953300/Yoga-teacher-training-2_ryrg6y.jpg" class="rounded-start img-fluid" alt="Group of yoga teachers">
                 </div>
                 <div class="col-md-8">
-                  <div class="card-body ms-3">
+                  <div class="ms-3 card-body">
                       <h5 class="card-title">Teachers</h5>
                       <p class="card-text">Meet the team! They are all certified and ready to welcome you in the yoga community </p>
                       <a href="teachers" class="btn btn-primary">Learn more</a>
@@ -46,7 +46,7 @@
         <p class="my-3 lead">What are you waiting for?</p>
         <p class="my-3">Come visit us at Via Ponzo 7, Milan, and experience the peaceful atmosphere of Yogatella</p>
         <div class="container">
-            <div class="row text-center align-items-center">
+            <div class="align-items-center text-center row">
                 <div class="col-sm">
                     <p>Via Ponzio, 7<br />
                     20131, Milano MI<br />
@@ -54,7 +54,7 @@
                     <a href="..." type="button" class="btn btn-primary" >Contact us</a>
                 </div>
                 <div class="col-sm">
-                    <img src="https://res.cloudinary.com/dpba22oef/image/upload/w_400/v1745955854/Screenshot_2025-04-18_alle_13.03.26_lwidg0.png" class="img-fluid rounded" alt="map">
+                    <img src="https://res.cloudinary.com/dpba22oef/image/upload/w_400/v1745955854/Screenshot_2025-04-18_alle_13.03.26_lwidg0.png" class="rounded img-fluid" alt="map">
                 </div>
             </div>
         </div>
