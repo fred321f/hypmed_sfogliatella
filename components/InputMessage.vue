e387f816e5ad522f6512fa67d31dfce702776cb7
SvevaZ <svevazanetti.sz@gmail.com>
2025-05-03 17:45:46 +0200

    Added contact link in the navigation bar and minor changes to contact page

--- components/InputMessage.vue
+++ components/InputMessage.vue
@@ -1,76 +1,73 @@
 <template>
     <div class="mb-5 container border border-2 border-primary">
-      <p class="my-3">Insert your email</p>
-      <div class="input-group mb-3">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">@</span>
+        <p class="my-3">Insert your email</p>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">@</span>
+            </div>
+            <input
+            type="text"
+            class="form-control"
+            placeholder="Example@mail.it"
+            aria-label="Example@mail.it"
+            aria-describedby="basic-addon1"
+            v-model="email"
+            />
         </div>
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Example@mail.it"
-          aria-label="Example@mail.it"
-          aria-describedby="basic-addon1"
-          v-model="email"
-        />
-      </div>
-      <p class="my-3">Write your message, then press <strong>Send Message</strong> button</p>
-      <div class="input-group">
-        <textarea
-          class="form-control"
-          placeholder="Your message"
-          aria-label="Text"
-          v-model="message"
-        ></textarea>
-      </div>
-
+        <p class="my-3">Write your message, then press <strong>Send Message</strong> button.</p>
+        <div class="input-group">
+            <textarea
+            class="form-control"
+            placeholder="Your message"
+            aria-label="Text"
+            v-model="message"
+            ></textarea>
+        </div>
         <div class="my-3">
-        <button
-            type="button"
-            class="btn btn-primary"
-            @click="handleSend"
-            :disabled="isSent"
-        >
-            {{ buttonText }}
-        </button>
+            <button
+                type="button"
+                class="btn btn-primary"
+                @click="handleSend"
+                :disabled="isSent"
+            >
+                {{ buttonText }}
+            </button>
         </div>
-
         <div v-if="showAlert" class="alert alert-warning" role="alert">
         Fill both email and message fields before sending!
         </div>
-  </div>
-  </template>
+    </div>
+</template>
 
-
-  <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        message: '',
-        buttonText: 'Send Message',
-        showAlert: false,
-        isSent: false,
-      };
-    },
-    computed: {
-      isFormValid() {
-        return this.email.trim() !== '' && this.message.trim() !== '';
-      },
-    },
-    methods: {
-      handleSend() {
-        if (!this.isFormValid) {
-          this.showAlert = true;
-          return;
-        }
-  
-        this.buttonText = 'Message Sent!';
-        this.email = '';
-        this.message = '';
-        this.showAlert = false;
-        this.isSent = true;
-      },
-    },
-  };
-  </script>
+<script>
+    export default {
+        data() {
+        return {
+            email: '',
+            message: '',
+            buttonText: 'Send Message',
+            showAlert: false,
+            isSent: false,
+        };
+        },
+        computed: {
+        isFormValid() {
+            return this.email.trim() !== '' && this.message.trim() !== '';
+        },
+        },
+        methods: {
+        handleSend() {
+            if (!this.isFormValid) {
+            this.showAlert = true;
+            return;
+            }
+    
+            this.buttonText = 'Message Sent!';
+            this.email = '';
+            this.message = '';
+            this.showAlert = false;
+            this.isSent = true;
+        },
+        },
+    };
+</script>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -6,17 +6,17 @@ import InputMessage from '~/components/InputMessage.vue'
 <template>
     <div class="mb-3 text-center container">
         <h1 class="my-5 text-center display-1">Contact us</h1>
-        <h2 class="my-3 text-center lead">Here are all the information you need to book your class, or just to get more information about the center or the activities.</h2>
+        <h2 class="my-3 text-center lead">Here are all the ways in which you can contact us to book your class, or just to get more information about the center or the activities we offer.</h2>
     </div>
     <div class="mb-5 container">
         <h3 class="my-3 display-6">MEET US IN PERSON!</h3>
-        <p class="my-3">You can talk directly to us in our center in Milan</p>
+        <p class="my-3">Visit our center in Milan, the receptionist will be ready to answer all of your questions and enroll you in your favourite activity. </p>
         <div class="align-items-center text-center row">
             <div class="col-sm-4">
                 <p>Via Ponzio, 7<br />
                 20131, Milano MI<br />
                 Italia</p>
-                <p>OPEN from monday to friday <br />
+                <p>OPEN from Monday to Friday <br />
                     from 8:00 to 19:00</p>
             </div>
             <div class="col-sm-8">
@@ -24,9 +24,9 @@ import InputMessage from '~/components/InputMessage.vue'
             </div>
         </div>
         <h3 class="my-3 display-6">ONLINE</h3>
-        <p class="my-3">You can send an email at <strong>yogatella@mail.it</strong> and we will reply as soon as possible</p>
-        <p class="my-3">You can send call as at <strong>+39 333 444 5555</strong> during our opening hours</p>
-        <p class="my-3">For facilitating the process, we prepareed the following form, you just need to insert your email and the message you want to send us, and we will reply in your inbox</p>
+        <p class="my-3">You can send an email at <strong>yogatella@mail.it</strong> and we will reply as soon as possible.</p>
+        <p class="my-3">You can call as at <strong>+39 333 444 5555</strong> during our opening hours from Monday to Friday from 8:00 to 19:00.</p>
+        <p class="my-3">For facilitating the process, we prepared the following form, you just need to insert your email and the message you want to send us, and we will reply in your inbox.</p>
     </div>
     <InputMessage />
 </template>
