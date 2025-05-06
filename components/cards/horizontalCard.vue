5b758151fc23c9dbfa894b44db9cd7c9cc4fd9e2
fred321f <frederick01nielsen@gmail.com>
2025-05-06 11:07:41 +0200

    updated global css

---
+++ components/cards/horizontalCard.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+import Button from '@/components/btns/mainBtn.vue';
+
+</script>
+
+<template>
+    <div class="shadow-lg m-3 border-0 rounded-4 w-100 overflow-hidden card activity-card">
+        <div class="overflow-hidden">
+            <img :src="this.imageUrl" class="card-img-top activity-img" />
+        </div>
+        <div class="d-flex flex-column card-body">
+            <h5 class="card-title fs-4 fw-bold">{{ this.title }}</h5>
+            <p class="text-secondary card-text" style="font-size: 16px; line-height: 2em">{{ this.description }}</p>
+            <div class="d-flex justify-content-center mt-auto row">
+
+                <Button :url="this.linkUrl" :text="this.buttonText" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// Props
+export default {
+    name: 'HorizontalCard',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        imageUrl: {
+            type: String,
+            required: true
+        },
+        linkUrl: {
+            type: String,
+            required: true
+        },
+        buttonText: {
+            type: String,
+            default: 'Read more'
+        }
+    }
+}
+</script>
