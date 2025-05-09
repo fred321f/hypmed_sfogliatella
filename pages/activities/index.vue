ef52f27885ae8835a95d8e30779daacd1c9e8ef5
SvevaZ <svevazanetti.sz@gmail.com>
2025-05-09 15:07:58 +0200

    changed text formatting

--- pages/activities/index.vue
+++ pages/activities/index.vue
@@ -7,7 +7,7 @@ import Overview from '~/components/ActivitiesOverview.vue';
 <template>
   <div class="container">
     <h1 class="my-5 text-center display-1">Activities</h1>
-    <p class="mb-5 text-center lead fs-4">
+    <p class="mb-5 text-center lead">
       At Yogatella Yoga Center, we offer a diverse range of activities designed to support your physical, mental, and
       spiritual well-being. Our offerings are thoughtfully grouped into three main categories:
     </p>
