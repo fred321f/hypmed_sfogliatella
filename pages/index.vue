021d3723052c680bb2947e78ca48f0d0f9c803c9
fred321f <frederick01nielsen@gmail.com>
2025-05-11 10:59:53 +0200

    Update index.vue
    removed comments

--- pages/index.vue
+++ pages/index.vue
@@ -10,9 +10,6 @@ import Button from '~/components/btns/mainBtn.vue'
         <div class="row hero-container">
             <div class="col-sm-6">
                 <h1 class="font-bold display-4 hero-header">At <span class="inline-brand">YogaTella</span>, we specialize in all types of yoga where you're in focus.</h1>
-                <!-- <h2 class="lead hero-paragraph">At the space where mind, body, and soul come together. Offering the
-                    highest quality services in Milan, we teach and host welfare events with a burning passion, where
-                    you are in the middle.</h2> -->
             </div>
             <div class="d-sm-block sm-display--block col-sm-6 d-none">
                 <img src="../assets/img/main_homepage-copy.jpg" class="rounded img-fluid hero-img"
@@ -20,23 +17,6 @@ import Button from '~/components/btns/mainBtn.vue'
             </div>
         </div>
         <div class="row"></div>
-
-
-
-
-        <!-- <h1 class="my-5 text-center display-1">YogaTella</h1>
-        <h2 class="my-3 text-center lead">Welcome to a space where mind, body, and soul come together.
-            Discover balance, strength, and serenity at our yoga center.
-        </h2>
-        <img src="https://res.cloudinary.com/dpba22oef/image/upload/v1745954116/header-yoga-business-in-a-nutshell-considerations-on-opening-your-own-yoga-studio_z7ymxo.jpg"
-            class="rounded img-fluid" alt="People doing yoga">
-        <p class="my-3 text-center">Yogatella is a well-established yoga center that has been offering the highest
-            quality services in Milan for many years. With a deep passion for well-being and a commitment to excellence,
-            we provide a welcoming space for all to explore the transformative power of yoga.
-        </p>
-        <div class="my-3 text-center">
-            <Button :url="'/about'" :text="'About YogaTella'" />
-        </div> -->
     </div>
     <div class="my-5 container">
         <Card type="horizontal" title="Activities"
